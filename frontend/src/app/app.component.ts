427fd402866318b1d373085c7c9422e4b3f3987d
Billel <billel.abbes@hotmail.com>
2024-10-23 16:05:36 +0200

    network

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit  {
   title = 'droranco-project';
+
+  backResponse = ''
+
+  public constructor(private httpClient : HttpClient){
+
+  }
+
+  ngOnInit(): void {
+     this.httpClient.get(
+       "http://localhost:8080",
+     ).subscribe(
+       (response : any ) => {
+         this.backResponse = response
+       }
+     )
+ }
 }
